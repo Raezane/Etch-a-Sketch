c43ed1eb76d73b47ef1291c1e0a018d197d5faa8
Antti Räsänen <143749752+Raezane@users.noreply.github.com>
2023-12-22 12:33:02 +0200

    add randomise color -functionality

--- javascript.js
+++ javascript.js
@@ -1,27 +1,34 @@
-function initialize() {
-
-  makeGrid()
-
-  };
-
-function makeGrid() {
+function main() {
 
   const maincontainer = document.querySelector('#maincontainer');
 
   let number = 15;
-  Grid(maincontainer, number)
+  makeGrid(maincontainer, number);
 
-  const askgrid = document.querySelector('.askgrid');
-  askgrid.addEventListener('click', () => {
+  const gridsize = document.querySelector('.gridsize');
+  gridsize.addEventListener('click', () => {
     //empty the parentNode (maincontainer) from all child elements
     maincontainer.textContent = '';
     let userInput = howManyDivs();
-    Grid(maincontainer, userInput);
+    makeGrid(maincontainer, userInput);
   });
+
+  const randomise = document.querySelector('.randomise');
+  let getColors = randomColors()
+  randomise.addEventListener('click', () => {
+    setColor();
+    //looppi tähän kuten makeGridissä
+  });
+
+  const clear = document.querySelector('.clear');
+  clear.addEventListener('click', () => {
+    ClearGrid(maincontainer);
+  });
+  
 };
 
-function Grid(maincontainer, number) {
 
+function makeGrid(maincontainer, number) {
 
   for (let row = 1; row <= number; row++) {
     for (let column = 1; column <= number; column++) {
@@ -30,11 +37,11 @@ function Grid(maincontainer, number) {
       let widthAndHeight = 600 / number + 'px';
       minibox.style.flexBasis = widthAndHeight;
       minibox.style.minHeight = widthAndHeight;
-
       minibox.addEventListener('mouseover', () => {
+
         minibox.style.backgroundColor = 'black';
       });
-
+      
       maincontainer.appendChild(minibox);
     
     };
@@ -42,6 +49,7 @@ function Grid(maincontainer, number) {
 };
 
 function howManyDivs() {
+
   while(true) {
     let result = prompt('Select the grid size between 1 and 100:')
     if (result >= 1 && result <= 100) {
@@ -50,9 +58,39 @@ function howManyDivs() {
         return;
     } else alert('You need to enter a value between 1 and 100!');
   };
+};
+
+function ClearGrid() {
+  let miniboxes = maincontainer.children;
+  for (let i = 0; i < miniboxes.length; i++) {
+    let clearMiniBox = miniboxes[i];
+    clearMiniBox.style.backgroundColor = 'white';
+  };
+};
+
+function setColor() {
+
+  let miniboxes = maincontainer.children;
+  for (let i = 0; i < miniboxes.length; i++) {
+    let randomiseMiniboxColor = miniboxes[i];
+    let colorValues = randomColors()
+    randomiseMiniboxColor.addEventListener('mouseover', () => {
+      randomiseMiniboxColor.style.backgroundColor = 'rgb(' + [colorValues[0], colorValues[1], colorValues[2]].join(',') + ')';
+  //return 'rgb(' + [colourList[0], colourList[1], colourList[2]].join(',') + ')';
+    });
+  };
+};
+
+function randomColors() {
+
+  let colors = [];
+
+  let red = Math.floor((Math.random() * 255) + 1);
+  let green = Math.floor((Math.random() * 255) + 1);
+  let blue = Math.floor((Math.random() * 255) + 1);
+
+  colors.push(red, green, blue);
+  return colors;
 
 }
-
-
-
-initialize()
+main();
