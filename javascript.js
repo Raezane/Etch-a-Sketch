d43b88e6aa468d6b1a1832a826c1efc790a68d92
Antti Räsänen <143749752+Raezane@users.noreply.github.com>
2023-12-30 15:11:38 +0200

    add functional shading and multicolor options

--- javascript.js
+++ javascript.js
@@ -1,94 +1,102 @@
 function main() {
 
   const maincontainer = document.querySelector('#maincontainer');
+  const number = 15;
 
-  let number = 15;
-  makeGrid(maincontainer, number);
-
+  const shade = document.querySelector('#shadecheck');
   const colorslct = document.querySelector('.colorslct');
-  colorslct.addEventListener('click', () => {
-    setColor(colorslct);
-  });
+
+  const randomise = document.querySelector('#rainbowcheck');
+
+  makeGrid(maincontainer, number);  
+  setColor(colorslct, shade, randomise);
+ 
 
   const gridsize = document.querySelector('.gridsize');
   gridsize.addEventListener('click', () => {
     //empty the parentNode (maincontainer) from all child elements
     maincontainer.textContent = '';
-    let userInput = howManyDivs();
+    let userInput = howManyDivs(number);
     makeGrid(maincontainer, userInput);
-    setColor(colorslct);
-  });
-  
-
-  const randomise = document.querySelector('.randomise');
-  let getColors = randomColor()
-  randomise.addEventListener('click', () => {
-    setRainbowColor();
-    //looppi tähän kuten makeGridissä
+    setColor(colorslct, shade, randomise);
   });
 
   const clear = document.querySelector('.clear');
   clear.addEventListener('click', () => {
     ClearGrid(maincontainer);
-    setColor(colorslct);
   });
 
 };
 
-function setColor(colorvalue) {
+function setColor(colorValue, shadeValue, randomiseValue) {
   let miniboxes = maincontainer.children;
   for (let i = 0; i < miniboxes.length; i++) {
+    let colorValues = randomColor();
     let MiniboxColor = miniboxes[i];
+    let switcher = true;
     MiniboxColor.addEventListener('mouseover', () => {
-      MiniboxColor.style.backgroundColor = colorvalue.value;
+      if (shadeValue.checked == true) {
+        randomiseValue.checked == false ? MiniboxColor.style.backgroundColor = colorValue.value : MiniboxColor.style.backgroundColor = 'rgb(' + [colorValues[0], colorValues[1], colorValues[2]].join(',') + ')';    
+          if (switcher) {
+            MiniboxColor.style.opacity = '0.1';
+            switcher = false;         
+        } else if (parseFloat(MiniboxColor.style.opacity) >= 0.0 && parseFloat(MiniboxColor.style.opacity) < 1.0 && switcher == false) {
+            let dimmer = parseFloat(MiniboxColor.style.opacity);
+            dimmer += 0.1;
+            MiniboxColor.style.opacity = String(dimmer);
+        };
+      }
+      else if (shadeValue.checked == false) { 
+          MiniboxColor.style.opacity = '1.0';
+          if (randomiseValue.checked == false) {
+            MiniboxColor.style.backgroundColor = colorValue.value;
+        } else if (randomiseValue.checked == true) {
+            MiniboxColor.style.backgroundColor = 'rgb(' + [colorValues[0], colorValues[1], colorValues[2]].join(',') + ')';
+        };
+      };
     });
   };
 };
-
 
 function makeGrid(maincontainer, number) {
 
   for (let row = 1; row <= number; row++) {
     for (let column = 1; column <= number; column++) {
-      const minibox = document.createElement('div');
 
+      const minibox = document.createElement('div');
       let widthAndHeight = 600 / number + 'px';
+      let switcher = true;
       minibox.style.flexBasis = widthAndHeight;
       minibox.style.minHeight = widthAndHeight;
-      minibox.addEventListener('mouseover', () => {
-        minibox.style.backgroundColor = 'black';
-      });
-      
       maincontainer.appendChild(minibox);
-    
     };
   };
 };
 
-function howManyDivs() {
+function howManyDivs(number) {
 
   while(true) {
-    let result = prompt('Select the grid size between 1 and 100:')
+    let result = prompt('Select the grid size between 1 and 100:');
     if (result >= 1 && result <= 100) {
       return parseInt(result);
     } else if (result === null) {
-        return;
+        return result = number;
     } else alert('You need to enter a value between 1 and 100!');
   };
 };
 
-function setRainbowColor() {
-
-
-  let miniboxes = maincontainer.children;
-  for (let i = 0; i < miniboxes.length; i++) {
-    let randomiseMiniboxColor = miniboxes[i];
-    let colorValues = randomColor()
-    randomiseMiniboxColor.addEventListener('mouseover', () => {
-      randomiseMiniboxColor.style.backgroundColor = 'rgb(' + [colorValues[0], colorValues[1], colorValues[2]].join(',') + ')';
-  //return 'rgb(' + [colourList[0], colourList[1], colourList[2]].join(',') + ')';
-    });
-  };
+function setRainbowColor(randomise, colorvalue, shadeValue) {
+  
+  if (randomise.checked == true) {
+    let miniboxes = maincontainer.children;
+    for (let i = 0; i < miniboxes.length; i++) {
+      let randomiseMiniboxColor = miniboxes[i];
+      let colorValues = randomColor();
+      randomiseMiniboxColor.addEventListener('mouseover', () => {
+        randomiseMiniboxColor.style.backgroundColor = 'rgb(' + [colorValues[0], colorValues[1], colorValues[2]].join(',') + ')';
+      });
+    };
+  } else setColor(colorvalue, shadeValue);
 };
 
 function randomColor() {
@@ -104,11 +112,7 @@ function randomColor() {
 };
 
 function ClearGrid() {
-  let miniboxes = maincontainer.children;
-  for (let i = 0; i < miniboxes.length; i++) {
-    let clearMiniBox = miniboxes[i];
-    clearMiniBox.style.backgroundColor = 'white';
-  };
+  location.reload()
 };
 
 main();
